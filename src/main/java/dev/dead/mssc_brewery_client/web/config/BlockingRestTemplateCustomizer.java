cb511b8f4c588ac9a1529c35dcde9f615ba61b14
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-06 01:38:26 +0300

    Rest Template Customizers

--- src/main/java/dev/dead/mssc_brewery_client/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/dev/dead/mssc_brewery_client/web/config/BlockingRestTemplateCustomizer.java
@@ -1,32 +1,28 @@
 package dev.dead.mssc_brewery_client.web.config;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.util.Timeout;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+
 import org.springframework.boot.web.client.RestTemplateCustomizer;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+// @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-  public ClientHttpRequestFactory clientHttpRequestFactory() {
+  private HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
     CloseableHttpClient httpClient = HttpClients.custom()
         .evictExpiredConnections()
         .build();
 
     HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-    factory.setConnectTimeout((int) Timeout.ofSeconds(3).toMilliseconds());
-    factory.setConnectionRequestTimeout((int) Timeout.ofSeconds(3).toMilliseconds());
-    factory.setReadTimeout((int) Timeout.ofSeconds(3).toMilliseconds());
 
     return factory;
   }
 
   @Override
   public void customize(RestTemplate restTemplate) {
-    restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    restTemplate.setRequestFactory(clientHttpRequestFactory());
   }
 }

0ef2d9a7a1780a2fdbf7a75f70a52eb6baf38c1b
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-06 01:41:41 +0300

    commenting configs

--- src/main/java/dev/dead/mssc_brewery_client/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/dev/dead/mssc_brewery_client/web/config/BlockingRestTemplateCustomizer.java
@@ -1,28 +1,28 @@
-package dev.dead.mssc_brewery_client.web.config;
+// package dev.dead.mssc_brewery_client.web.config;
 
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+// import org.apache.hc.client5.http.impl.classic.HttpClients;
+// import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 
-import org.springframework.boot.web.client.RestTemplateCustomizer;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.boot.web.client.RestTemplateCustomizer;
+// import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.client.RestTemplate;
 
-// @Component
-public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+// // @Component
+// public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-  private HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
-    CloseableHttpClient httpClient = HttpClients.custom()
-        .evictExpiredConnections()
-        .build();
+//   private HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+//     CloseableHttpClient httpClient = HttpClients.custom()
+//         .evictExpiredConnections()
+//         .build();
 
-    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//     HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
-    return factory;
-  }
+//     return factory;
+//   }
 
-  @Override
-  public void customize(RestTemplate restTemplate) {
-    restTemplate.setRequestFactory(clientHttpRequestFactory());
-  }
-}
+//   @Override
+//   public void customize(RestTemplate restTemplate) {
+//     restTemplate.setRequestFactory(clientHttpRequestFactory());
+//   }
+// }
--- src/main/java/dev/dead/mssc_brewery_client/web/config/RestTemplateConfig.java
+++ src/main/java/dev/dead/mssc_brewery_client/web/config/RestTemplateConfig.java
@@ -1,20 +1,20 @@
-package dev.dead.mssc_brewery_client.web.config;
+// package dev.dead.mssc_brewery_client.web.config;
 
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+// import org.apache.hc.client5.http.impl.classic.HttpClients;
+// import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+// import org.springframework.web.client.RestTemplate;
 
-// @Configuration
-public class RestTemplateConfig {
+// // @Configuration
+// public class RestTemplateConfig {
 
-    // @Bean
-    public RestTemplate restTemplate() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//     // @Bean
+//     public RestTemplate restTemplate() {
+//         CloseableHttpClient httpClient = HttpClients.createDefault();
+//         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
-        return new RestTemplate(factory);
-    }
-}
+//         return new RestTemplate(factory);
+//     }
+// }
